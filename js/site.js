21f698a0d502ad8a1e20d3cb87ab68455aec6db3
attrib <attrib@attrib.org>
2013-12-08 13:49:11 +0100

    Added English version

--- js/site.js
+++ js/site.js
@@ -1,16 +1,36 @@
-/**
- * JQuery Part
- */
+(function($, document, window) {
 $(document).ready(function(){
   $(".main").onepage_scroll({
     sectionContainer: "section",
-    responsiveFallback: 600
+    responsiveFallback: 600,
+    pagination: false,
+    loop: true,
+    afterMove: function(next_page_index) {
+      var real_index = $('section.active').data('index'),
+        menu = $('#menu');
+      menu.find('.active').removeClass('active');
+      if (real_index > 1) {
+        menu.show();
+        menu.find('a:eq(' + (real_index - 2) + ')').addClass('active');
+      }
+      else {
+        menu.hide();
+      }
+    }
   });
 
+  $('#menu').hide().find("a").click(function() {
+    event.preventDefault();
+    $(".main").moveTo($(this).data('index'))
+  });
+
+  /**
+   * Google Maps Integration
+   */
   var position = new google.maps.LatLng(50.1193833, 8.6502);
   var mapOptions = {
     center: position,
-    zoom: 8
+    zoom: 10
   };
   var map = new google.maps.Map(document.getElementById("gmap"),
     mapOptions);
@@ -23,3 +43,4 @@ $(document).ready(function(){
   });
 
 });
+})(jQuery, document, window)
